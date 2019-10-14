31c905248be2fe70322b32cf81cfe7e603cd0c43
simonkreienbaum <s.kreienbaum@gmail.com>
2019-10-14 15:36:31 +0200

    Remove account_id from api_update
    Calling api_update with a new account id doesn't do anything as the paramater immutable.

--- facebook_business/adobjects/adcreative.py
+++ facebook_business/adobjects/adcreative.py
@@ -284,7 +284,6 @@ class AdCreative(
         if batch is None and (success is not None or failure is not None):
           api_utils.warning('`success` and `failure` callback only work for batch call.')
         param_types = {
-            'account_id': 'string',
             'adlabels': 'list<Object>',
             'name': 'string',
             'status': 'status_enum',
